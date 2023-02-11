02940eedba34436522f9a1e77bf54d07adbaeb94
fajar <fajarrasyid564@gmail.com>
2023-02-11 11:33:20 +0700

    first commit

---
+++ evaluasi.ts
@@ -0,0 +1,57 @@
+// let array = ["fulan", true, 2023];
+// console.log(array[2]);
+
+// let nama = {
+//   mobile: "fajar,fatih,yusup",
+//   fronend: "sanca,syahdin,marko",
+//   backend: "argha,ramdan,lukman",
+// };
+// console.log(nama.mobile);
+
+// soal nomor 8
+
+// var number: any = prompt("silahkan masukan angka");
+// if (number % 2 === 0) {
+//   alert(number + "  bilangan genap ");
+// } else {
+//   alert(number + "  bilangan ganjil ");
+// }
+
+// let jawab: any = prompt(
+//   "silahkan plih menu yang ada:bakso,sate,naspat,daging ayam"
+// );
+// let harga: string = "";
+// switch (jawab) {
+//   case "daging ayam":
+//     harga = "daging ayam 70.000";
+//     break;
+
+//   case "naspat":
+//     harga = " naspat 50.000";
+//     break;
+
+//   case "bakso":
+//     harga = " bakso 40.0000";
+//     break;
+
+//   case "sate":
+//     harga = "harga sate 30.000";
+//     break;
+// }
+// if (harga == "") {
+//   alert("menu tidak ada");
+// } else {
+//   alert(" harga " + harga);
+// }
+// let umur: any = prompt("berapa umurmu?");
+// if (umur <= 10) {
+//   alert("anak-anak");
+// } else if (umur <= 18) {
+//   alert("remaja");
+// } else if (umur <= 35) {
+//   alert("dewasa");
+// } else if (umur <= 65) {
+//   alert("paruhbaya");
+// } else {
+//   alert("tua");
+// }
